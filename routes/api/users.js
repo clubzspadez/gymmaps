3f0c7b7ed8d1163f526955b8cac13c9032857650
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-06-12 19:43:45 -0400

    adding bcrypt function to hash password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
+// ! Dependencies
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 /**
  * !Import User Model
@@ -24,12 +27,34 @@ router.post("/register", (req, res) => {
       if (user) {
         return res.status(400).json({ email: "Email exists" });
       } else {
+        const rounds = 10;
+
+        //* gravatar has two methods (url, and profile_url)
+        //* both methods take parameters such as
+        //* gravatar.url(email, options) || gravatar.url(email, options, protocol);
+        //? for me info @ https://github.com/emerleite/node-gravatar
+
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm"
+        });
+
+        //* generate newUser model object and assign
+        //* the data that is being parsed from the body
+
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
           avatar: avatar,
           password: req.body.password
         });
+
+        //* Generate bcrypt hash for password
+        //* genSalt(rounds, [optional]minor, [optional]cb);
+        //? https://github.com/kelektiv/node.bcrypt.js
+
+        bcrypt.genSalt(rounds);
       }
     })
     .catch();
