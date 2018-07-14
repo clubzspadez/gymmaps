29859b5f6a2216a4ebbeae6b3123adb0cc48337c
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-07-14 13:00:31 -0400

    router.post private route for current profile. Using out JWT we can auth current user profile and create the necessary profile fields for that user. After setting the requests from the body to our necessary fields we can then find the current user and update there profile with profileInfo. If the user handle doesn't exist we can create a new Profile model, pass in the profileInfo object, and then save that information.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+//! client requests https://en.wikipedia.org/wiki/List_of_HTTP_status_codes#4xx_Client_errors
+
 /**
  * !Import Profile Model and User Model
  */
@@ -43,35 +45,61 @@ router.post(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     //! Profile Object for Profile Details
-    const profile = {};
-    profile.user = req.user.id;
-    if (req.body.handle) profile.handle = req.body.handle;
-    if (req.body.experience) profile.experience = req.body.experience;
-    if (req.body.website) profile.website = req.body.website;
-    if (req.body.bio) profile.bio = req.body.bio;
-    if (req.body.location) profile.location = req.body.location;
-    if (req.body.status) profile.status = req.body.status;
-    if (req.body.company) profile.company = req.body.company;
-    if (req.body.status) profile.status = req.body.status;
+    const profileInfo = {};
+    profileInfo.user = req.user.id;
+    if (req.body.handle) profileInfo.handle = req.body.handle;
+    if (req.body.experience) profileInfo.experience = req.body.experience;
+    if (req.body.website) profileInfo.website = req.body.website;
+    if (req.body.bio) profileInfo.bio = req.body.bio;
+    if (req.body.location) profileInfo.location = req.body.location;
+    if (req.body.status) profileInfo.status = req.body.status;
+    if (req.body.company) profileInfo.company = req.body.company;
+    if (req.body.status) profileInfo.status = req.body.status;
 
     //! Social Object
-    profile.social = {};
-    if (req.body.youtube) profile.youtube = req.body.youtube;
-    if (req.body.twitter) profile.twitter = req.body.twitter;
-    if (req.body.linkedin) profile.linkedin = req.body.linkedin;
-    if (req.body.insta) profile.insta = req.body.insta;
-    if (req.body.facebook) profile.facebook = req.body.facebook;
+    profileInfo.social = {};
+    if (req.body.youtube) profileInfo.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileInfo.social.twitter = req.body.twitter;
+    if (req.body.linkedin) profileInfo.social.linkedin = req.body.linkedin;
+    if (req.body.insta) profileInfo.social.insta = req.body.insta;
+    if (req.body.facebook) profileInfo.social.facebook = req.body.facebook;
 
+    // check if skills is an array
     if (typeof req.body.skills !== "undefined") {
-      profile.skills = req.body.skills.split(",");
+      profileInfo.skills = req.body.skills.split(",");
     }
 
+    const profileIdentifier = { user: req.body.user };
     // ! Update profile with profiles objects for the user
-    Profile.findOne({ user: req.body.user }).then(profile => {
+    Profile.findOne(profileIdentifier).then(profile => {
       // ! If the profile exists update that profile
-      // * using findOneAndUpdate method
+      // * using findOneAndUpdate method -> findOneAndUpdate(conditions, update, options, callback)
+      // ? http://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
       if (profile) {
+        Profile.findOneAndUpdate(
+          // condition/query - >{}
+          profileIdentifier,
+          // update -> {}
+          { $set: profileInfo },
+          // options --> {} new: bool - if true, return the modified document rather than the original. defaults to false (changed in 4.0)
+          { new: true }
+        ).then(profile => {
+          // send response with json data of profile
+          res.json(profile);
+        });
       } else {
+        // Create profile if one does not exist
+        // to do so validate the use/url handle
+        Profile.findOne({ handle: profileInfo.handle }).then(profile => {
+          // if the handle exists return json error
+          if (profile) {
+            errors.handle = "Handle already exists";
+            //return client 400 bad request and json with error
+            res.status(400).json(errors);
+          }
+          // create new profile
+          new Profile(profileInfo).save().then(() => res.json(profile));
+        });
       }
     });
   }
