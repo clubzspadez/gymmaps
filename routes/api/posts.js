1985190ba8a033738ec764674e98d2e40e77cb43
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-08-25 20:38:47 -0400

    adding validation and router posts / on post page. Fixing validation, and posting

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
-
+const Post = require('../../models/Post');
 router.get("/test", (req, res) => res.json({ test: "this is working" }));
 
 /**
@@ -18,6 +18,25 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {}
+  // validation
+  const { errors, isValid } = validatePostData(req.body);
+
+    //* check if isValid is false
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+
+  // schema
+  const newPost = new Post({
+    text: req.body.text,
+    name:req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  }
+); 
+
+  newPost.save().then( post => res.json(post));
 );
 
 module.exports = router;
