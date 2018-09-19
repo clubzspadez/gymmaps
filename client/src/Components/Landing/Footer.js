61d5ae70a98b988e785fc4e78cca65df7785d4bf
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-09-18 20:49:38 -0400

    adding on to frontend portion of app

--- client/src/Components/Landing/Footer.js
+++ client/src/Components/Landing/Footer.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { get } from "http";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copyRight: new Date().getFullYear()
+    };
+  }
+
   render() {
     return (
       <footer>
-        <p>Hoxha and Co, Copyright &copy;</p>
+        <p>Hoxha and Co, Copyright &copy; {this.state.copyRight}</p>
       </footer>
     );
   }
