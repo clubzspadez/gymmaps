484fac795a3e5afda21e699b80153c09c63e5caa
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-09-26 19:56:50 -0400

    working with input fields by setting default values from state

--- client/src/Components/Auth/Register.js
+++ client/src/Components/Auth/Register.js
@@ -20,39 +20,36 @@ class Register extends Component {
           <span className="highlight2">M</span>{" "}
         </h1>
         <h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
-        <label htmlFor="inputEmail" className="sr-only" />
         <input
           type="text"
-          id="inputName"
           className="form-control"
           placeholder="First and Last Name"
+          name="name"
+          value={this.state.name}
           required
           autoFocus
         />
-        <label htmlFor="inputEmail" className="sr-only" />
         <input
           type="email"
-          id="inputEmail"
           className="form-control"
           placeholder="Email address"
-          required
+          name="email"
+          value={this.state.email}
           autoFocus
         />
-        <label htmlFor="inputPassword" className="sr-only" />
         <input
           type="password"
-          id="inputPassword"
           className="form-control"
           placeholder="Password"
-          required
+          name="password"
+          value={this.state.password}
         />
-        <label htmlFor="password2" className="sr-only" />
         <input
           type="password"
-          id="inputPassword2"
           className="form-control"
           placeholder="Re-enter Password"
-          required
+          name="password2"
+          value={this.state.password2}
         />
         <div className="checkbox mb-3">
           <label>
