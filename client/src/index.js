b54046f1b442b5c38be2a5697b35138d379ed189
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-10-08 22:42:41 -0400

    partially working on importing middleware into createStore. Need to address more of this tomorrow with good sleep

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// https://redux.js.org/api/createstore
+// takes a reducer(function), preloaded state(any), and middleware(function)
+const store = createStore(() => {}, [], applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
