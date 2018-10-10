7d85768f4b863067b7c49eb631dad9f9941d358c
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-10-09 21:58:45 -0400

    added in reducers folder for our reducers, root reducer file, and created root component for provider and app

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,29 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import reducers from "../src/reducers/index";
 
+const state = {};
 // https://redux.js.org/api/createstore
 // takes a reducer(function), preloaded state(any), and middleware(function)
-const store = createStore(() => {}, [], applyMiddleware(thunk));
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+const store = createStore(
+  reducers,
+  state,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
